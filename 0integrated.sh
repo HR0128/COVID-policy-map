05789fe6fe40946a9c09ebf39d83fb0231117d1b
HR0128 <76907561+HR0128@users.noreply.github.com>
2022-03-18 06:19:57 -0400

    Add files via upload

---
+++ 0integrated.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH--time=55:00
+#SBATCH--workdir=/gpfs/home/sr2364/
+
+module purge
+module load py-pandas-1.1.5-gcc-9.3.0-7ahntqt
+module load anaconda3-2020.07-gcc-9.3.0-myrjwlf
+module load anaconda3-2020.07-gcc-9.3.0-myrjwlf
+module load anaconda3-2020.07-gcc-9.3.0-myrjwlf
+/opt/ohpc/admin/spack/spack/opt/spack/linux-centos8-zen2/gcc-9.3.0/anaconda3-2020.07-myrjwlfwrtrgdgpx6si4tcslrytnytnl/bin/python3.8 /gpfs/home/sr2364/map_html_generation.py
